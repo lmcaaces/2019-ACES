0cad4033c1c2f904353a7a3fddf67106ca2a4ab3
Kevin O. Grover <kevin@kevingrover.net>
2019-02-17 13:11:06 -0800

    Changed package to frc.team6957

--- src/main/java/frc/team6957/Robot.java
+++ src/main/java/frc/team6957/Robot.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot;
+package frc.team6957;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
